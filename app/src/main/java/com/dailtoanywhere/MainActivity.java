f13def1f48c18619a0c03a07bfe8b8773de1f557
Faisal Khan <faisalkhan1690@gmail.com>
2015-10-29 00:51:38 +0530

    :Boom started

---
+++ app/src/main/java/com/dailtoanywhere/MainActivity.java
@@ -0,0 +1,18 @@
+package com.dailtoanywhere;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * created by fasial khan
+ */
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/dailtoanywhere/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.dailtoanywhere;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by faisal khan
+ */
+public class SplashActivity extends Activity {
+    private static final long SPLASH_HOLD_TIME =2000 ;// 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(SplashActivity.this,LoginActiviity.class));
+            }
+        }, SPLASH_HOLD_TIME);
+    }
+
+}
